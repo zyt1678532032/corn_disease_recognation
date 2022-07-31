b3f5d81c33f614ae00fad4412c716b3c615bbe1c
张彦通 <1678532032@qq.com>
2022-07-31 21:19:52 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # corn_disease_recognation
+
+#### 使用TensorFlow Lite将深度学习模型转化后开发玉米病害图像识别应用。
+
+1. 使用相机拍照或者选择本地相册的图像进行识别
+2. 对图像进行预处理以符合模型的图像输入大小
+3. 使用AndroidX Camera库操作相机
+4. 使用TensorFlow Lite中ImageProcessor将Bitmap处理为TensorImage，BaseOptions设置线程来加速模型的计算
---
+++ app/src/main/java/org/tensorflow/lite/examples/imageclassification/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.tensorflow.lite.examples.imageclassification
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tensorflow.lite.examples.imageclassification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
